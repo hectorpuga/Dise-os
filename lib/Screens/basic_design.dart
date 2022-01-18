872566bc86c36b53c04f08e1a257c15bdd8182c1
Hector Puga <pugagarciah.@gmail.com>
2022-01-18 00:27:27 -0600

    Se añadio 3 botones, y un texto,con los cuales el diseño quedo completo

--- lib/Screens/basic_design.dart
+++ lib/Screens/basic_design.dart
@@ -7,8 +7,14 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        Image(image: AssetImage('assets/landscape.jpg')),
-        Title()
+        const Image(image: AssetImage('assets/landscape.jpg')),
+        const Title(),
+        const ButtonSection(),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+              'Aliqua sint veniam ad dolore sunt nulla esse anim aliquip deserunt. Proident pariatur do laboris et irure culpa irure reprehenderit ipsum in sit mollit dolore. Commodo sit occaecat non in voluptate est fugiat deserunt exercitation. Ut consequat mollit ipsum velit labore quis sint minim enim.'),
+        )
       ]),
     );
   }
@@ -47,6 +53,57 @@ class Title extends StatelessWidget {
           const Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const [
+          CustomButton(
+            icono: Icons.call,
+            texto: 'CALL',
+          ),
+          CustomButton(
+            icono: Icons.send,
+            texto: 'ROUTE',
+          ),
+          CustomButton(
+            icono: Icons.share,
+            texto: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icono;
+  final String texto;
+  const CustomButton({Key? key, required this.icono, required this.texto})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icono, color: Colors.blue[400]),
+        const SizedBox(height: 5),
+        Text(
+          texto,
+          style: TextStyle(color: Colors.blue[400]),
+        ),
+      ],
     );
   }
 }
